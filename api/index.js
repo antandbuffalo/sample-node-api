0856797917a7753faf8fcd708db6b74f473519b4
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-10-07 21:43:20 +0530

    initial commit for vercel

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const port = process.env.PORT || 3002;
+const express = require("express");
+
+let app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Respond to preflight request
+  if (req.method === "OPTIONS") {
+    res.status(200);
+    res.end();
+    return;
+  }
+  next();
+});
+
+app.get("/", async function (req, res) {
+  res.send({
+    status: "success",
+  });
+});
+
+module.exports = app;
+
+// app.listen(port);
+
+// console.log(`running on ${port}`);
